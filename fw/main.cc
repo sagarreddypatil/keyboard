32535228d69f4ecc6c774a391555ebdfae1e3c3f
Sagar Patil <sagarreddypatil@gmail.com>
2025-10-15 02:07:48 -0700

    working

--- fw/main.cc
+++ fw/main.cc
@@ -1,132 +1,199 @@
 #include "config.h"
 #include "util.h"
 
-#include <bsp/board_api.h>
+#include <bsp/board.h>
 #include <hardware/gpio.h>
-#include <pico/platform/common.h>
-#include <pico/stdio_usb.h>
-#include <pico/time.h>
+#include <stdlib.h>
 #include <tusb.h>
 
-u8 keycodes[6]{};
-u8 prev_keycodes[6]{};
-u8 modifiers = 0;
-u8 prev_modifiers = 0;
-
-void matrix_init()
+class Keyboard
 {
-    for (u32 i = 0; i < kRows; ++i)
+public:
+    Keyboard()
     {
-        const u32 pin = kRowPins[i];
-        gpio_init(pin);
-        gpio_set_dir(pin, GPIO_IN);
-        gpio_pull_down(pin);
     }
-    for (u32 i = 0; i < kCols; ++i)
-    {
-        const u32 pin = kColPins[i];
-        gpio_init(pin);
-        gpio_set_dir(pin, GPIO_OUT);
-        gpio_put(pin, false);
-    }
-}
+    Keyboard(const Keyboard &) = delete;
+    Keyboard(Keyboard &&) = delete;
 
-void matrix_scan()
-{
-    u32 i = 0;
-    modifiers = 0;
-
-    for (u32 ci = 0; ci < kCols; ++ci)
+    void init()
     {
-        const u32 col = kColPins[ci];
-        gpio_put(col, true);
-        sleep_us(1);
+        for (u32 i = 0; i < kRows; ++i)
         {
-            for (u32 ri = 0; ri < kRows; ++ri)
-            {
-                const u32 row = kRowPins[ri];
-                if (gpio_get(row))
-                {
-                    const u8 kc = kKeymap[ri][ci];
-                    printf("key row=%d col=%d name=%s\n", ri, ci, kKeymapStr[ri][ci]);
+            const u32 pin = kRowPins[i];
+            gpio_init(pin);
+            gpio_set_dir(pin, GPIO_IN);
+            gpio_pull_down(pin);
+        }
+        for (u32 i = 0; i < kCols; ++i)
+        {
+            const u32 pin = kColPins[i];
+            gpio_init(pin);
+            gpio_set_dir(pin, GPIO_OUT);
+            gpio_put(pin, false);
+        }
+    }
 
-                    if (kc >= HID_KEY_CONTROL_LEFT && kc <= HID_KEY_GUI_RIGHT)
+    void scan()
+    {
+        u32 i = 0;
+        m_modifiers = 0;
+
+        for (u32 ci = 0; ci < kCols; ++ci)
+        {
+            const u32 col = kColPins[ci];
+            gpio_put(col, true);
+            sleep_us(1);
+            {
+                for (u32 ri = 0; ri < kRows; ++ri)
+                {
+                    const u32 row = kRowPins[ri];
+                    if (gpio_get(row))
                     {
-                        modifiers |= (1 << (kc - HID_KEY_CONTROL_LEFT));
-                    }
-                    else if (i < ARRAY_SIZE(keycodes))
-                    {
-                        keycodes[i++] = kc;
+                        const u8 kc = kKeymap[ri][ci];
+                        printf("key row=%d col=%d name=%s\n", ri, ci, kKeymapStr[ri][ci]);
+
+                        if (kc >= HID_KEY_CONTROL_LEFT && kc <= HID_KEY_GUI_RIGHT)
+                        {
+                            m_modifiers |= (1 << (kc - HID_KEY_CONTROL_LEFT));
+                        }
+                        else if (i < ARRAY_SIZE(m_keycodes))
+                        {
+                            m_keycodes[i++] = kc;
+                        }
                     }
                 }
             }
+            gpio_put(col, false);
         }
-        gpio_put(col, false);
-    }
-    for (; i < ARRAY_SIZE(keycodes); ++i)
-    {
-        keycodes[i] = 0;
-    }
-}
-
-bool keys_changed()
-{
-    if (modifiers != prev_modifiers)
-        return true;
-
-    for (u32 i = 0; i < ARRAY_SIZE(keycodes); ++i)
-    {
-        if (keycodes[i] != prev_keycodes[i])
-            return true;
-    }
-    return false;
-}
-
-void send_hid_report()
-{
-    if (tud_suspended() && keys_changed())
-    {
-        tud_remote_wakeup();
-        return;
-    }
-    if (tud_hid_ready() && keys_changed())
-    {
-        tud_hid_keyboard_report(0x01, modifiers, keycodes);
-        prev_modifiers = modifiers;
-        for (u32 i = 0; i < ARRAY_SIZE(keycodes); ++i)
+        for (; i < ARRAY_SIZE(m_keycodes); ++i)
         {
-            prev_keycodes[i] = keycodes[i];
+            m_keycodes[i] = 0;
         }
     }
-}
 
-int main()
+    bool keys_changed() const
+    {
+        if (m_modifiers != m_prev_modifiers)
+            return true;
+
+        for (u32 i = 0; i < ARRAY_SIZE(m_keycodes); ++i)
+        {
+            if (m_keycodes[i] != m_prev_keycodes[i])
+                return true;
+        }
+        return false;
+    }
+
+    bool keys_pressed() const
+    {
+        if (m_modifiers)
+        {
+            return true;
+        }
+        for (u32 i = 0; i < ARRAY_SIZE(m_keycodes); ++i)
+        {
+            if (m_keycodes[i] != 0)
+                return true;
+        }
+        return false;
+    }
+
+    void next()
+    {
+        m_prev_modifiers = m_modifiers;
+        m_modifiers = 0;
+        for (u32 i = 0; i < ARRAY_SIZE(m_keycodes); ++i)
+        {
+            m_prev_keycodes[i] = m_keycodes[i];
+            m_keycodes[i] = 0;
+        }
+    }
+
+    u8 m_keycodes[6]{};
+    u8 m_modifiers = 0;
+
+private:
+    u8 m_prev_keycodes[6]{};
+    u8 m_prev_modifiers = 0xFF;
+
+    static_assert(ARRAY_SIZE(m_keycodes) == ARRAY_SIZE(m_prev_keycodes));
+};
+
+Keyboard kb;
+
+void hid_task();
+
+int main(void)
 {
     board_init();
     tusb_init();
-    if (board_init_after_tusb)
-    {
-        board_init_after_tusb();
-    }
-    stdio_init_all();
 
-    matrix_init();
-
-    for (u32 i = 0; i < ARRAY_SIZE(prev_keycodes); ++i)
-    {
-        prev_keycodes[i] = 0xFF;
-    }
-    prev_modifiers = 0xFF;
-
-    while (true)
+    kb.init();
+    while (1)
     {
         tud_task();
-
-        matrix_scan();
-        send_hid_report();
-
-        sleep_ms(10);
+        hid_task();
     }
+
+    return 0;
+}
+
+static void send_hid_report()
+{
+    // skip if hid is not ready yet
+    if (!tud_hid_ready())
+    {
+        return;
+    }
+
+    if (kb.keys_pressed())
+    {
+        tud_hid_keyboard_report(0x01, kb.m_modifiers, kb.m_keycodes);
+    }
+    else
+    {
+        // send empty key report if previously has key pressed
+        if (kb.keys_changed())
+        {
+            tud_hid_keyboard_report(0x01, 0, NULL);
+        }
+    }
+}
+
+void hid_task()
+{
+    // Poll every 10ms
+    const uint32_t interval_ms = 10;
+    static uint32_t start_ms = 0;
+
+    if (board_millis() - start_ms < interval_ms)
+    {
+        return; // not enough time
+    }
+    start_ms += interval_ms;
+
+    // Check for keys pressed
+    kb.scan();
+    bool const keys_pressed = kb.keys_pressed();
+
+    // Remote wakeup
+    if (tud_suspended() && keys_pressed)
+    {
+        // Wake up host if we are in suspend mode
+        // and REMOTE_WAKEUP feature is enabled by host
+        tud_remote_wakeup();
+    }
+    else
+    {
+        // send a report
+        send_hid_report();
+    }
+}
+
+void tud_hid_report_complete_cb(uint8_t instance, uint8_t const *report, uint8_t len)
+{
+    (void)instance;
+    (void)len;
 }
 
 uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type,
@@ -150,9 +217,19 @@ void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_
     (void)bufsize;
 }
 
-void tud_hid_report_complete_cb(uint8_t instance, uint8_t const *report, uint16_t len)
+void tud_mount_cb(void)
 {
-    (void)instance;
-    (void)report;
-    (void)len;
+}
+
+void tud_umount_cb(void)
+{
+}
+
+void tud_suspend_cb(bool remote_wakeup_en)
+{
+    (void)remote_wakeup_en;
+}
+
+void tud_resume_cb(void)
+{
 }
--- fw/usb_descriptors.c
+++ fw/usb_descriptors.c
@@ -1,6 +1,29 @@
-#include "tusb.h"
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Ha Thach (tinyusb.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
 
-#include <pico/unique_id.h>
+#include "tusb.h"
 
 /* A combination of interfaces must have a unique product id, since PC will save device driver after
  * the first plug. Same VID/PID with different interface e.g MSC (first), then CDC (later) will
@@ -14,29 +37,29 @@
     (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | _PID_MAP(MIDI, 3) |         \
      _PID_MAP(VENDOR, 4))
 
-#define USB_VID 0xfbae
+#define USB_VID 0xcafe
 #define USB_BCD 0x0200
 
 //--------------------------------------------------------------------+
 // Device Descriptors
 //--------------------------------------------------------------------+
-static const tusb_desc_device_t desc_device = {.bLength = sizeof(tusb_desc_device_t),
-                                               .bDescriptorType = TUSB_DESC_DEVICE,
-                                               .bcdUSB = USB_BCD,
-                                               .bDeviceClass = TUSB_CLASS_UNSPECIFIED,
-                                               .bDeviceSubClass = 0x00,
-                                               .bDeviceProtocol = 0x00,
-                                               .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
+tusb_desc_device_t const desc_device = {.bLength = sizeof(tusb_desc_device_t),
+                                        .bDescriptorType = TUSB_DESC_DEVICE,
+                                        .bcdUSB = USB_BCD,
+                                        .bDeviceClass = 0x00,
+                                        .bDeviceSubClass = 0x00,
+                                        .bDeviceProtocol = 0x00,
+                                        .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
 
-                                               .idVendor = USB_VID,
-                                               .idProduct = USB_PID,
-                                               .bcdDevice = 0x0100,
+                                        .idVendor = USB_VID,
+                                        .idProduct = USB_PID,
+                                        .bcdDevice = 0x0100,
 
-                                               .iManufacturer = 0x01,
-                                               .iProduct = 0x02,
-                                               .iSerialNumber = 0x03,
+                                        .iManufacturer = 0x01,
+                                        .iProduct = 0x02,
+                                        .iSerialNumber = 0x03,
 
-                                               .bNumConfigurations = 0x01};
+                                        .bNumConfigurations = 0x01};
 
 // Invoked when received GET DEVICE DESCRIPTOR
 // Application return pointer to descriptor
@@ -49,7 +72,7 @@ uint8_t const *tud_descriptor_device_cb(void)
 // HID Report Descriptor
 //--------------------------------------------------------------------+
 
-static const uint8_t desc_hid_report[] = {TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(0x01))};
+uint8_t const desc_hid_report[] = {TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(0x01))};
 
 // Invoked when received GET HID REPORT DESCRIPTOR
 // Application return pointer to descriptor
@@ -85,8 +108,54 @@ uint8_t const desc_configuration[] = {
                        CFG_TUD_HID_EP_BUFSIZE, 5)};
 
 #if TUD_OPT_HIGH_SPEED
-#error tried to use USB high speed
-#endif
+// Per USB specs: high speed capable device must report device_qualifier and
+// other_speed_configuration
+
+// other speed configuration
+uint8_t desc_other_speed_config[CONFIG_TOTAL_LEN];
+
+// device qualifier is mostly similar to device descriptor since we don't change configuration based
+// on speed
+tusb_desc_device_qualifier_t const desc_device_qualifier = {
+    .bLength = sizeof(tusb_desc_device_qualifier_t),
+    .bDescriptorType = TUSB_DESC_DEVICE_QUALIFIER,
+    .bcdUSB = USB_BCD,
+
+    .bDeviceClass = 0x00,
+    .bDeviceSubClass = 0x00,
+    .bDeviceProtocol = 0x00,
+
+    .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
+    .bNumConfigurations = 0x01,
+    .bReserved = 0x00};
+
+// Invoked when received GET DEVICE QUALIFIER DESCRIPTOR request
+// Application return pointer to descriptor, whose contents must exist long enough for transfer to
+// complete. device_qualifier descriptor describes information about a high-speed capable device
+// that would change if the device were operating at the other speed. If not highspeed capable stall
+// this request.
+uint8_t const *tud_descriptor_device_qualifier_cb(void)
+{
+    return (uint8_t const *)&desc_device_qualifier;
+}
+
+// Invoked when received GET OTHER SEED CONFIGURATION DESCRIPTOR request
+// Application return pointer to descriptor, whose contents must exist long enough for transfer to
+// complete Configuration descriptor in the other speed e.g if high speed then this is for full
+// speed and vice versa
+uint8_t const *tud_descriptor_other_speed_configuration_cb(uint8_t index)
+{
+    (void)index; // for multiple configurations
+
+    // other speed config is basically configuration with type = OHER_SPEED_CONFIG
+    memcpy(desc_other_speed_config, desc_configuration, CONFIG_TOTAL_LEN);
+    desc_other_speed_config[1] = TUSB_DESC_OTHER_SPEED_CONFIG;
+
+    // this example use the same configuration for both high and full speed mode
+    return desc_other_speed_config;
+}
+
+#endif // highspeed
 
 // Invoked when received GET CONFIGURATION DESCRIPTOR
 // Application return pointer to descriptor
@@ -104,11 +173,11 @@ uint8_t const *tud_descriptor_configuration_cb(uint8_t index)
 //--------------------------------------------------------------------+
 
 // array of pointer to string descriptors
-const char *string_desc_arr[] = {
+char const *string_desc_arr[] = {
     (const char[]){0x09, 0x04}, // 0: is supported language is English (0x0409)
-    "Sagar",                    // 1: Manufacturer
-    "Keyboard 0",               // 2: Product
-    "PLACEHOLDER",              // 3: Serials, should use chip ID
+    "rktrlng",                  // 1: Manufacturer
+    "TinyUSB keyboard",         // 2: Product
+    "123456",                   // 3: Serials, should use chip ID
 };
 
 static uint16_t _desc_str[32];
@@ -118,10 +187,6 @@ static uint16_t _desc_str[32];
 // complete
 uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid)
 {
-    char id_str[2 * PICO_UNIQUE_BOARD_ID_SIZE_BYTES + 1];
-    pico_get_unique_board_id_string(id_str, sizeof(id_str));
-    string_desc_arr[3] = id_str;
-
     (void)langid;
 
     uint8_t chr_count;
